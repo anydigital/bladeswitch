42cf5beb3e068f0159d60fae1caf1ab102c7151f
Anton Staroverov <73071319+tonystar@users.noreply.github.com>
2026-02-27 15:09:23 -0600

    refactor: Internalize breakout CSS from an external package to a local file and remove `postcss-preset-env`.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
-const breakoutConfig = require('./node_modules/@anydigital/breakout-css/postcss.config.js');
-
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    ...breakoutConfig.plugins,
-  }
-}
+    "postcss-import": {},
+  },
+};
